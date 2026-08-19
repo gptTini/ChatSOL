e529e374b41c003bf2e888619d63cfda9291ffc4
gptTini <jttini74@gmail.com>
2026-08-20 00:59:29 +0900

    test: expand adversarial remote URL coverage

--- tests/test_repo_ref.py
+++ tests/test_repo_ref.py
@@ -22,6 +22,24 @@ class RepoRefTests(unittest.TestCase):
             RepoRef("gptTini", "ChatSOL"),
         )
 
+    def test_ssh_scheme_url(self):
+        self.assertEqual(
+            parse_repo_ref("ssh://git@github.com/gptTini/ChatSOL.git"),
+            RepoRef("gptTini", "ChatSOL"),
+        )
+
+    def test_git_scheme_url(self):
+        self.assertEqual(
+            parse_repo_ref("git://github.com/gptTini/ChatSOL.git"),
+            RepoRef("gptTini", "ChatSOL"),
+        )
+
+    def test_github_host_is_case_insensitive(self):
+        self.assertEqual(
+            parse_repo_ref("https://GITHUB.com/gptTini/ChatSOL.git"),
+            RepoRef("gptTini", "ChatSOL"),
+        )
+
     def test_reject_non_github_https_host(self):
         with self.assertRaises(ValueError):
             parse_repo_ref("https://evil.example/gptTini/ChatSOL.git")
@@ -29,6 +47,14 @@ class RepoRefTests(unittest.TestCase):
     def test_reject_non_github_ssh_host(self):
         with self.assertRaises(ValueError):
             parse_repo_ref("git@evil.example:gptTini/ChatSOL.git")
+
+    def test_reject_deceptive_github_host(self):
+        with self.assertRaises(ValueError):
+            parse_repo_ref("https://github.com.evil.example/gptTini/ChatSOL.git")
+
+    def test_reject_unsupported_url_scheme(self):
+        with self.assertRaises(ValueError):
+            parse_repo_ref("ftp://github.com/gptTini/ChatSOL.git")
 
     def test_reject_extra_path_components(self):
         with self.assertRaises(ValueError):
